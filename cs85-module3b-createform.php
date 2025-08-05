538691644c0493ac0b60304a8a8918f427724b9f
Jos Cashon <joscashon@gmail.com>
2025-08-04 21:16:26 -0700

    Initial commit: starter HTML

---
+++ cs85-module3b-createform.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Form Starter</title>
+</head>
+<body>
+
+<?php
+// TODO: PHP form handling logic
+?>
+
+</body>
+</html>
